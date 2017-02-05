55cb7fc371e2bb5a41f050f1431b03938ad569c8
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-05 21:02:38 +0100

    add ssi include for product list

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('combined'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.status(200).render('index')
+})
 
 app.use((req, res, next) =>
   res.status(404).render('404'))
 
-app.use((error, req, res, next) =>
-  res.status(500).render('500', { error }))
+app.use((error, req, res, next) => {
+  console.log(error)
+  res.status(500).render('500', { error })
+})
 
 console.log(`start listening on port ${process.env.PORT}`)
 app.listen(process.env.PORT)
